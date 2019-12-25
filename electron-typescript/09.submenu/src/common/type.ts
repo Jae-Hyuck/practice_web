9851fa89a24d6ada03438ca361b1d4910d5e1cde
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2019-12-26 02:29:50 +0900

    Add 09.submenu

---
+++ electron-typescript/09.submenu/src/common/type.ts
@@ -0,0 +1,12 @@
+export interface LoginObjType {
+    email: string;
+    password: string;
+}
+
+export interface MessageObjectType {
+    id: string;
+    email: string;
+    name: string;
+    message: string;
+    time: string;
+}
